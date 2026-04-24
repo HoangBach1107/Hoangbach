749d3cbe344836ef66230df48d9f40a0936ed343
Hoangbach1107 <hoangbachhp1107@gmail.com>
2026-04-25 00:15:43 +0700

    add recommend endpoint

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from main import run
 
 app = FastAPI()
 
+# test server
 @app.get("/")
 def home():
     return {"message": "API running"}
 
-@app.get("/predict")   # ⚠️ PHẢI CÓ DÒNG NÀY
-def predict():
+# API chính
+@app.get("/recommend")
+def recommend():
     return run()
