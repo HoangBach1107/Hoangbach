89ada7510a76f7dcdde1ed4f8874a6899a499f47
Hoangbach1107 <hoangbachhp1107@gmail.com>
2026-04-25 00:19:16 +0700

    fix: add recommend API

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ from main import run
 
 app = FastAPI()
 
-# test server
 @app.get("/")
 def home():
     return {"message": "API running"}
 
-# API chính
 @app.get("/recommend")
 def recommend():
     return run()
